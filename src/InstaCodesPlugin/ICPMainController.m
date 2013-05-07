9673f2c8ffa2851655e2b04fb27c081c59934262
Christopher <cleetruman@gmail.com>
2013-05-07 16:16:01 -0700

    Open in default browser
    No need to detect browser, just open in default browser.

--- src/InstaCodesPlugin/ICPMainController.m
+++ src/InstaCodesPlugin/ICPMainController.m
@@ -71,30 +71,8 @@ NSString * const kMenuItemTitle = @"Post Selection to Instacode";
         
         NSArray * browsers = [self installedBrowsers];
         NSMenuItem * newMenuItem = nil;
-        
-        if ([browsers count] < 2)
-        {
-            newMenuItem = [[NSMenuItem alloc] initWithTitle:kMenuItemTitle action:NULL keyEquivalent:@""];
-            [newMenuItem setTarget:self];
-            [newMenuItem setRepresentedObject:[browsers lastObject]];
-        }
-        else
-        {
-            newMenuItem = [[NSMenuItem alloc] initWithTitle:kMenuItemTitle action:NULL keyEquivalent:@""];
-            NSMenu * browsersMenu = [[NSMenu alloc] initWithTitle:kMenuItemTitle];
-            
-            for (NSString *browserID in browsers)
-            {
-                NSString *itemTitle = [NSString stringWithFormat:@"Post using %@", [[[browserID componentsSeparatedByString:@"."] lastObject] capitalizedString]];
-                NSMenuItem * browserMenuItem = [[NSMenuItem alloc] initWithTitle:itemTitle action:@selector(postToInstacodes:) keyEquivalent:@""];
-                [browserMenuItem setTarget:self];
-                [browserMenuItem setRepresentedObject:browserID];
-                [browsersMenu addItem:browserMenuItem];
-            }
-            
-            [newMenuItem setSubmenu:browsersMenu];
-        }
-    
+        newMenuItem = [[NSMenuItem alloc] initWithTitle:kMenuItemTitle action:NULL keyEquivalent:@""];
+        [newMenuItem setTarget:self];
         [[editMenuItem submenu] addItem:newMenuItem];
         [newMenuItem release];
     }
@@ -123,63 +101,10 @@ NSString * const kMenuItemTitle = @"Post Selection to Instacode";
 - (void)postToInstacodes:(id)sender
 {
     NSLog(@"[InstaCodesPlugin] Posting to Instacod.es:\n%@", self.currentSelection);
+    NSString * postCode = [self.currentSelection URLEncodedString];
+    NSString * URLString = [NSString stringWithFormat:@"http://instacod.es/?post_code=%@&post_lang=%@", postCode, @"ObjC"];
     
-    NSString * browserID = [sender representedObject];
-    
-    if (browserID == nil)
-    {
-        [[NSAlert alertWithMessageText:@"No browsers" defaultButton:@"OK" alternateButton:nil otherButton:nil
-            informativeTextWithFormat:@"No WebGL supporting browsers installed on your system"] runModal];
-    }
-    else
-    {
-        BOOL webGLEnabled = YES;
-        
-        if ([browserID isEqualToString:kBrowserBundleIDSafari])
-        {
-            // Check if Safari supports WebGL
-            BOOL supportsWebGL = [[[[NSUserDefaults standardUserDefaults] persistentDomainForName:kBrowserBundleIDSafari]
-                objectForKey:kSafariPrefsWebGLSupportKey] boolValue];
-            
-            if (!supportsWebGL)
-            {
-                [[NSAlert alertWithMessageText:@"WebGL support is disabled in Safari by default"
-                    defaultButton:@"OK" alternateButton:nil otherButton:nil
-                    informativeTextWithFormat:@"Instacod.es requires browser that supports WebGL to work properly. To enable WebGL in Safari, "
-                    "go to Safari Preferences -> Advanced tab, check 'Show Develop menu in menu bar'. "
-                    "Then open Develop menu and check Enable WebGL menu item."] runModal];
-                webGLEnabled = NO;
-            }
-        }
-        
-        if (webGLEnabled)
-        {
-            NSString * postCode = [self.currentSelection URLEncodedString];
-            NSString * URLString = [NSString stringWithFormat:@"http://instacod.es/?post_code=%@&post_lang=%@", postCode, @"ObjC"];
-            
-            [[NSWorkspace sharedWorkspace] openURLs:@[[NSURL URLWithString:URLString]] withAppBundleIdentifier:browserID
-                options:NSWorkspaceLaunchDefault additionalEventParamDescriptor:nil launchIdentifiers:NULL];
-        }
-    }
-}
-
-#pragma mark - Private methods -
-
-- (NSArray *)installedBrowsers
-{
-    NSMutableArray * browsers = [NSMutableArray arrayWithObjects:kBrowserBundleIDSafari, kBrowserBundleIDChrome,
-        kBrowserBundleIDFirefox,/* kBrowserBundleIDOpera ,*/ nil];
-    NSWorkspace * workspace = [NSWorkspace sharedWorkspace];
-    
-    for (NSString * browserID in browsers)
-    {
-        if (nil == [workspace absolutePathForAppBundleWithIdentifier:browserID])
-        {
-            [browsers removeObject:browserID];
-        }
-    }
-    
-    return browsers;
+    [[NSWorkspace sharedWorkspace] openURL:@[[NSURL URLWithString:URLString]]];
 }
 
 - (void)debugAlertWithMessage:(NSString *)message
