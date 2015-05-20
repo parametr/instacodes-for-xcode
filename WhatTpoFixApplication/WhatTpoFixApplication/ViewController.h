5d30dd3d8b1e58fd59c41a19342e31618aa0ff4a
Joachim Kurz <joachim@monoceres.uberspace.de>
2015-05-20 13:42:55 +0200

    added application

---
+++ WhatTpoFixApplication/WhatTpoFixApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatTpoFixApplication
+//
+//  Created by Joachim Kurz on 20.05.15.
+//  Copyright (c) 2015 Joachim Kurz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ WhatTpoFixApplication/WhatTpoFixApplication/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WhatTpoFixApplication
+//
+//  Created by Joachim Kurz on 20.05.15.
+//  Copyright (c) 2015 Joachim Kurz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
