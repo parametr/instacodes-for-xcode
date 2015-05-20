63f11b84817a10a1db7060e89222b007c3c1158c
Joachim Kurz <joachim@monoceres.uberspace.de>
2015-05-20 14:04:18 +0200

    setup for shared constants

---
+++ WhatToFixApplication/WhatToFixApplication/WTFNotificationReceiver.m
@@ -0,0 +1,26 @@
+//
+//  WTFNotificationReceiver.m
+//  WhatToFixApplication
+//
+//  Created by Joachim Kurz on 20.05.15.
+//  Copyright (c) 2015 What To Fix. All rights reserved.
+//
+
+#import "WTFNotificationReceiver.h"
+#import "WTFSharedConstants.h"
+
+@interface WTFNotificationReceiver ()
+
+@property NSOperationQueue *operationQueue;
+
+@end
+
+@implementation WTFNotificationReceiver
+
+- (void)awakeFromNib
+{
+    self.operationQueue = [NSOperationQueue mainQueue];
+    [[NSDistributedNotificationCenter defaultCenter] addObserverForName:WTFNewNotificationKey object:nil queue:<#(NSOperationQueue *)#> usingBlock:<#^(NSNotification *note)block#>]
+}
+
+@end
---
+++ WhatToFixApplication/WhatToFixApplication/WTFSharedConstants.h
@@ -0,0 +1,15 @@
+//
+//  WTFSharedConstants.h
+//  WhatToFixApplication
+//
+//  Created by Joachim Kurz on 20.05.15.
+//  Copyright (c) 2015 What To Fix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString* const WTFNewNotificationKey;
+
+@interface WTFSharedConstants : NSObject
+
+@end
